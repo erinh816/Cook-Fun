77a1906958cfa1ad33500ab77e7bb519b7a4f42f
Silu Hu <erinh816@gmail.com>
2024-02-28 15:09:09 -0500

    figured out how to access api key by adding dotenv configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 import express from 'express';
 import cors from 'cors';
+//import all from that file and bind to obj RecipeAPI
+import * as RecipeAPI from './recipe-api';
+// import searchRecipes from './recipe-api.ts';
 
 const app = express();
 
@@ -8,7 +13,14 @@ app.use(cors());
 
 
 app.get("/api/recipes/search", async(req, res)=>{
-  res.json({message:'success!'})
+  // res.json({message:'success!'})
+
+  //will need to add error handling to make sure the value of searchTerm and page exists
+  const searchTerm = req.query.searchTerm as string;
+  const page = parseInt(req.query.page as string);
+  const results = await RecipeAPI.searchRecipes(searchTerm, page);
+  
+  return res.json(results)
 })
 
 app.listen(5000, ()=>{
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,2 +1,37 @@
 
-const apiKey = process.env.API_KEY
+const apiKey = process.env.API_KEY;
+
+
+export const searchRecipes = async (searchTerm:string, page:number) => {
+  console.log("apiKey is",apiKey)
+  if(!apiKey){
+    throw new Error("API Key not found")
+  }
+
+  //step1: getting the end point
+  const url = new URL("https://api.spoonacular.com/recipes/complexSearch")
+  
+  const queryParams = {
+    apiKey: apiKey,
+    query:searchTerm,
+    number:'10',
+    offset:(page*10).toString()
+  }
+
+  //search property starts with "?""
+  url.search = new URLSearchParams(queryParams).toString()
+
+  //step2: making the request
+  try{
+    const searchResponse = await fetch(url);
+    const resultsJson = await searchResponse.json();
+    return resultsJson;
+  }catch(error){
+    console.log(error)
+  }
+}
+
+// export default searchRecipes;
+
+//step3: call this function in get request in index.ts
+  //import function there
