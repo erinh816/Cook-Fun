b919083172741554eef0ed42a39eafd6ca2dd33a
Silu Hu <erinh816@gmail.com>
2024-02-29 10:01:18 -0500

    tested displaying recipe properties, hard coded searchTerm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,34 @@
-import {useState} from 'react';
+import {useState, FormEvent} from 'react';
 import "./App.css";
 import * as api from './api';
+import {Recipe} from './types';
+
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("burgers");
   //the results from calling api is an array of a bunch of recipe objects, so we initialize the recipes as empty array
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState<Recipe[]>([])
 
-  const handleSearchSubmit = async ()=>{
+ 
+
+  const handleSearchSubmit = async (evt:FormEvent)=>{
+    evt.preventDefault();
     try{
       const recipes = await api.searchRecipes(searchTerm,1);
-      setRecipes(recipes);
+      setRecipes(recipes.results);
     }catch(error){
       console.log(error)
     }
   }
 
+
+
   return(
     <div>
+      <form onSubmit={handleSearchSubmit}>
+      <button type="submit">Submit</button>
+      </form>
+      <p>hello</p>
       {recipes.map((recipe)=>(
         <div>
           <p>{recipe.title}</p>
