ecab3922970857fa3ef5864bb5263d51c45cd7f0
Silu Hu <erinh816@gmail.com>
2024-03-13 16:52:53 -0400

    added the click function for view more and it works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {useState, FormEvent, ChangeEvent} from 'react';
+import {useState, useRef, useEffect, FormEvent, ChangeEvent} from 'react';
 import "./App.css";
 import * as api from './api';
 import {Recipe} from './types';
@@ -10,25 +10,70 @@ function App() {
   //the results from calling api is an array of a bunch of recipe objects, so we initialize the recipes as empty array
   const [recipes, setRecipes] = useState<Recipe[]>([])
 
+  const pageNumber = useRef(1);
+
  
 
+  /* for seperate of concerns, the fetch functionality */ 
   const handleSearchSubmit = async (evt:FormEvent)=>{
     evt.preventDefault();
     try{
-      const recipes = await api.searchRecipes(searchTerm,1);
-      setRecipes(recipes.results);
+      // const recipes = await api.searchRecipes(searchTerm,1);
+      const response = await api.searchRecipes(searchTerm,1);
+      // setRecipes(recipes.results);
+      setRecipes(response.results);
     }catch(error){
       console.log(error)
     }
   }
-
+/** 
   // function onChange(evt:ChangeEvent<HTMLInputElement>){
   //   setSearchTerm(evt.target.value)
   // }
-  function handleViewMoreClick(){
+  async function handleViewMoreClick(){
     //get offset number, ex:page is 2, offset is 20
     //add functionality to skip 20 and load 10 more?
+
+    //call search recipe API, pass in the query and current page
+    const nextPage = pageNumber.current + 1;
+
+    try{
+      // const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
+      // console.log('nextrecipes value is', nextRecipes.results)
+      // //so next we will use 
+      // setRecipes([...recipes, ...nextRecipes])
+      // console.log("check current recipes",recipes)
+
+      const response = await api.searchRecipes(searchTerm, nextPage);
+    // Extract the results array from the response
+    const nextRecipes = response.results;
+
+    // Update the recipes state by concatenating the existing recipes with the new ones
+    setRecipes([...recipes, ...nextRecipes]);
+      console.log("check current recipes",recipes)
+    }catch(error){
+      console.log(error)
+    }
+
   }
+  */
+  async function handleViewMoreClick() {
+    const nextPage = pageNumber.current + 1;
+
+    try {
+        const response = await api.searchRecipes(searchTerm, nextPage);
+        const nextRecipes = response.results;
+
+        setRecipes([...recipes, ...nextRecipes]);
+        console.log('current current wowowow', recipes)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+useEffect(() => {
+    console.log("check current recipes", recipes);
+}, [recipes]);
 
 
   return(
